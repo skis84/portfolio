5c3eea49bf7e48d9b84fff716e033f0f427d37ad
Sanna Luukkonen <sannsivo@gmail.com>
2020-06-17 16:16:28 +0300

    Testing touch event

--- gallery.js
+++ gallery.js
@@ -56,7 +56,7 @@ const images = sources.map((source, index) => {
   image.addEventListener("load", () => fade(image));
   image.addEventListener("click", () => animate(index));
   image.addEventListener("touchstart", () => animate(index));
-  image.addEventListener("touch", () => animate(index));
+  image.addEventListener("touch", () => alert(index));
   toggle(image).src = `img/gallery/${source}`;
 
   list.append(item);
