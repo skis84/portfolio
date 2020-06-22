b560c9c447cd2e869ddf572cb3737775ae33be4c
Sanna Luukkonen <sannsivo@gmail.com>
2020-06-22 13:26:23 +0300

    Gallery

--- gallery.js
+++ gallery.js
@@ -122,12 +122,7 @@ window.addEventListener("keyup", ({ key }) => {
 });
 
 // Add image list to main
-/*const main = document.getElementById("gallery");
+const main = document.getElementById("gallery");
 if (main) {
   main.append(list);
-}*/
-
-const { body } = document;
-if (body) {
-  body.append(list);
 }
